cf074a6dcc11c356110eec7441a10bcbb2ca6f47
RichardOkongo <rirachard19@gmail.com>
2023-01-05 14:52:36 +0300

    initial changes to js file

---
+++ js/index.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("#github-form");
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
